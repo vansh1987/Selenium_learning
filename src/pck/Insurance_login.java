0e21b02b02cdf542b6088f04f0aecb43581f6e57
Vansh <Vanshstyle1987@gmail.com>
2017-01-23 18:53:42 +0530

    Selenium script login

---
+++ src/pck/Insurance_login.java
@@ -0,0 +1,48 @@
+package pck;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Insurance_login 
+{
+	
+	WebDriver driver = new FirefoxDriver();
+	
+	public void openurl()
+	{
+		driver.get("https://www.pocketclinik.com/login");
+	}
+	
+	public void login() throws Exception
+	{
+		driver.findElement(By.name("email")).sendKeys("uhj68380@dsiay.com");
+		Thread.sleep(6000);
+		
+		driver.findElement(By.name("password")).sendKeys("123456");
+		
+		driver.findElement(By.xpath(".//*[@id='app-layout']/div[1]/div/div[2]/div/div/div/div/form/div[4]/button")).click();
+		Thread.sleep(3000);
+	}
+	
+	public void clickinglinks(String str) throws Exception
+	{
+		driver.findElement(By.xpath(str));
+	}
+	
+	public void logout() throws Exception
+	{
+		driver.findElement(By.className(".//*[@id='app-layout']/div[2]/div[1]/div/div/div/div/ul/li/a/div")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(".//*[@id='app-layout']/div[2]/div[1]/div/div/div/div/ul/li/ul/li[2]/a")).click();
+	}
+	public static void main(String[] args) throws Exception
+	{
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Demo\\Downloads\\geckodriver-v0.13.0-win64\\geckodriver.exe");
+		Insurance_login i = new Insurance_login();
+		i.openurl();
+		i.login();
+		i.logout();
+	}
+
+}
---
+++ src/pck/Login_1.java
@@ -0,0 +1,11 @@
+package pck;
+
+public class Login_1 {
+
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+}
